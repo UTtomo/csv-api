6d6d6df893337f25add034f05cfa9e19877970fa
Tomoki Kawano <tomoki.ut.ssps@gmail.com>
2018-12-08 14:03:04 +0900

    add comment for explanation

--- imageupload.py
+++ imageupload.py
@@ -7,27 +7,46 @@ import wiringpi as pi
 import time
 import mcp_adc
 
-
+# declare variable
 print('Data collecting program start')
+
+# setting parameters for AD converter
 SPI_CE = 0
 SPI_SPEED = 1000000
 READ_CH = 0
 VREF = 3.3
+
+# Storage parameters above to "adc" 
 adc = mcp_adc.mcp3208( SPI_CE, SPI_SPEED, VREF )
+
+# Get time and date and storage it to "now"
 now = datetime.datetime.now()
 City = 'citycity'
 
 
 fmt_name = "python-data/test_{0:%Y%m%d-%H%M%S}.csv".format(now)
+
 print('start collecting data...')
 
+# edit text with open-method
+# open(file location,mode).
+# shorten open-method as "f" 
 with open(fmt_name,'w') as f:
+
+    # edit the file in range of 0 to 50 of i.
     for i in range(0,50):
         now = datetime.datetime.now()
         value = adc.get_value( READ_CH )
+
+        # lineteriminator will be set at the end of each row.
         writer = csv.writer(f, lineterminator='\n')
+
+        # write each row with this command
         writer.writerow([now,value,City])
+
+        # sumpling frequency. set the number in parenthesis here
         time.sleep( 0.1 )
+
 f.close()
 
 
@@ -35,6 +54,7 @@ print( 'New file has created. Its name is ')
 print(fmt_name)
 print('    ')
 
+# commands necessary for Google Drive
 gauth = GoogleAuth()
 gauth.CommandLineAuth()
 drive = GoogleDrive(gauth)
